f414b1a93dff0ecaf772adb6dbc72ba26ac2aafb
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-05 17:57:04 +0100

    completed milestone 3

--- script.js
+++ script.js
@@ -11,15 +11,15 @@ function printCard(postcard){
   let {title, url} = postcard;
 
   document.getElementById('row-card').innerHTML += 
-        `<div class="col col-lg-4 col-md-6 col-sm-12 mb-4 px-3">
-          <div class="card p-1 my-2">
+      `<div class= "col col-lg-4 col-md-6 col-sm-12 mb-4 px-3">
+        <div class="card p-3 my-2 bg-white">
             <div class="card-img-container">
               <img src="img/pin.svg" class="pin" alt="pin">
               <img src="${url}" class="card-img-top" alt="img" style="object-fit: cover; width: 100%; height: 100%;">
             </div>
             <div class="card-body">
               <p class="card-text">${title}</p>
-            </div>
-          </div>
+            </div>  
+        </div>
         </div>`;
       }
