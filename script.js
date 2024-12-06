0d43d4ec67a33e824dc29dbd905a255aaa7f3aaf
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-06 16:42:03 +0100

    aggiunta logica di funzionamento e commenti vari sul funzionamento di ogni riga/elemento

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
+// URL dell'API
 const apiUrl = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
+
+// Elemento HTML dove verranno aggiunte le card
 let cardDaStampare = document.getElementById('row-card');
 
+// Effettuo una richiesta GET all'API utilizzando Axios
 axios.get(apiUrl)
 .then(res => {
+// Itero sui dati ricevuti dalla risposta dell'API e creo una card per ciascun oggetto
   res.data.forEach(postcard => printCard(postcard))
-  const overlay = document.querySelector('.overlay');
+
+// Variabili
+  const overlay = document.querySelector('.overlay');// Contenitore con l'immagine ingrandita
   const closeOverlayButton = document.getElementById('close-overlay');
   const galleryImages = document.querySelectorAll('.card-img-top');
   const overlayImage = document.getElementById('overlay-image');
-        
+
+// Aggiungo un event listener a ciascuna immagine nella galleria e utilizzo src per copiare il collegamento     
 galleryImages.forEach(image => {
   image.addEventListener('click', () => {
     overlayImage.src = image.src;
+// Mostro l'overlay aggiungendo la classe `d-block`
     overlay.classList.add('d-block');
   });
 });
 
+// Aggiungo un event listener al pulsante di chiusura dell'overlay
 closeOverlayButton.addEventListener('click', () => {
+// Nascondo nuovamente l'overlay rimuovendo la classe `d-block`
   overlay.classList.remove('d-block');
   });
 })
 
+// Creo una funzione per aggiungere una card dentro il contenitore row-card
 function printCard(postcard){
+
+// Destrutturo l'oggetto postcard per ottenere `title` e `url`  
   let {title, url} = postcard;
 
+// Aggiungo una nuova card all'elemento HTML
 document.getElementById('row-card').innerHTML += 
 `<div class= "col col-lg-4 col-md-6 col-sm-12 mb-4 px-3">
   <div class="card p-3 my-2 bg-white">
@@ -37,3 +52,7 @@ document.getElementById('row-card').innerHTML +=
   </div>
 </div>`;
 }
+
+// L'obiettivo finale di questo codice è caricare le immagini da un'API e mostrarle come card.
+// Quando clicco un'immagine, questa viene mostrata ingrandita in un overlay.
+// L'overlay può essere chiuso con un pulsante.
