2bb73430b3533f2f5b12948aa1210adc87ff7172
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-06 16:26:41 +0100

    overlayImage.src per copiare il collegamento nell'image.src

--- script.js
+++ script.js
@@ -7,35 +7,33 @@ axios.get(apiUrl)
   const overlay = document.querySelector('.overlay');
   const closeOverlayButton = document.getElementById('close-overlay');
   const galleryImages = document.querySelectorAll('.card-img-top');
+  const overlayImage = document.getElementById('overlay-image');
         
-  galleryImages.forEach(image => {
-    console.log('forEach')
+galleryImages.forEach(image => {
   image.addEventListener('click', () => {
-      overlay.classList.add('d-block');
-    });
+    overlayImage.src = image.src;
+    overlay.classList.add('d-block');
   });
+});
 
-  closeOverlayButton.addEventListener('click', () => {
-    overlay.classList.remove('d-block');
+closeOverlayButton.addEventListener('click', () => {
+  overlay.classList.remove('d-block');
   });
 })
 
 function printCard(postcard){
   let {title, url} = postcard;
 
-  document.getElementById('row-card').innerHTML += 
-  `<div class= "col col-lg-4 col-md-6 col-sm-12 mb-4 px-3">
-    <div class="card p-3 my-2 bg-white">
-        <div class="card-img-container">
-          <img src="img/pin.svg" class="pin" alt="pin">
-          <img src="${url}" class="card-img-top" alt="img" style="width: 100%; height: 100%;">
-        </div>
-        <div class="card-body">
-          <p class="card-text">${title}</p>
-        </div>  
-    </div>
-  </div>`;
+document.getElementById('row-card').innerHTML += 
+`<div class= "col col-lg-4 col-md-6 col-sm-12 mb-4 px-3">
+  <div class="card p-3 my-2 bg-white">
+      <div class="card-img-container">
+        <img src="img/pin.svg" class="pin" alt="pin">
+        <img src="${url}" class="card-img-top" alt="img" style="width: 100%; height: 100%;">
+      </div>
+      <div class="card-body">
+        <p class="card-text">${title}</p>
+      </div>  
+  </div>
+</div>`;
 }
-  
-
-      
