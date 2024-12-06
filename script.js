1a6b805e7db25d1475ba81ca61b4afd38e8d48ab
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-06 15:19:41 +0100

    fix adeventlistener

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
-let cardDaStampare = document.getElementById('row-card')
+let cardDaStampare = document.getElementById('row-card');
+
+const overlay = document.getElementById('overlay');
+const closeOverlayButton = document.getElementById('close-overlay');
+const galleryImages = document.querySelectorAll('.card-img-top');
+
+galleryImages.forEach(image => {
+  image.addEventListener('click', () => {
+    overlay.classList.add('overlay');
+  });
+});
+
+closeOverlayButton.addEventListener('click', () => {
+  overlay.classList.remove('overlay');
+});
 
 axios.get(apiUrl)
 .then(res => {
@@ -14,7 +28,7 @@ function printCard(postcard){
         <div class="card p-3 my-2 bg-white">
             <div class="card-img-container">
               <img src="img/pin.svg" class="pin" alt="pin">
-              <img src="${url}" class="card-img-top" alt="img" style="object-fit: cover; width: 100%; height: 100%;">
+              <img src="${url}" class="card-img-top" alt="img" style="width: 100%; height: 100%;">
             </div>
             <div class="card-body">
               <p class="card-text">${title}</p>
@@ -22,3 +36,5 @@ function printCard(postcard){
         </div>
         </div>`;
       }
+  
+      
