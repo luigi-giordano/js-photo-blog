362fd55df471341e489f8a43843dd13dac190026
luigi-giordano <luigi.giordano1992@gmail.com>
2024-12-10 15:28:03 +0100

    add comments to order code

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
+// L'obiettivo finale di questo codice è caricare le immagini da un'API e mostrarle come card.
+
+// Flusso di lavoro:
+// 1 Una richiesta all'API scarica le immagini.
+// 2 Le immagini vengono mostrate come card.
+// 3 Cliccando un'immagine, si apre un overlay con l'immagine ingrandita.
+// 4 Chiudiamo l'overlay cliccando sul pulsante di chiusura.
+
 // URL dell'API
 const apiUrl = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
 
-// Elemento HTML dove verranno aggiunte le card
+// Seleziono l'elemento HTML dove verranno aggiunte le card
 let cardDaStampare = document.getElementById('row-card');
 
-// Effettuo una richiesta GET all'API utilizzando Axios
+// Effettuo una richiesta GET all'API utilizzando Axios, la quale preleva i dati dall'API indicata
 axios.get(apiUrl)
 .then(res => {
-// Itero sui dati ricevuti dalla risposta dell'API e creo una card per ciascun oggetto
-  res.data.forEach(postcard => printCard(postcard))
+
+// Creo una funzione per aggiungere una card dentro il contenitore row-card
+function printCard(postcard){
+
+  // Destrutturo l'oggetto postcard per ottenere title e url  
+    let {title, url} = postcard;
+  
+  // Aggiungo una nuova card all'elemento HTML
+  document.getElementById('row-card').innerHTML += 
+  `<div class= "col col-lg-4 col-md-6 col-sm-12 mb-4 px-3">
+    <div class="card p-3 my-2 bg-white">
+        <div class="card-img-container">
+          <img src="img/pin.svg" class="pin" alt="pin">
+          <img src="${url}" class="card-img-top" alt="img" style="width: 100%; height: 100%;">
+        </div>
+        <div class="card-body">
+          <p class="card-text">${title}</p>
+        </div>  
+    </div>
+  </div>`;
+  }
+
+// Ottenuti i dati, li itero richiamando la funzione printCard che li stampa dentro il contenitore
+res.data.forEach(postcard => printCard(postcard))
 
 // Variabili
-  const overlay = document.querySelector('.overlay');// Contenitore con l'immagine ingrandita
-  const closeOverlayButton = document.getElementById('close-overlay');
-  const galleryImages = document.querySelectorAll('.card-img-top');
-  const overlayImage = document.getElementById('overlay-image');
+  const galleryImages = document.querySelectorAll('.card-img-top');// Seleziono le immagini visibili nelle card
+  const overlayImage = document.getElementById('overlay-image');// Seleziono l'immagine ingradita dell'overlay
+  const overlay = document.querySelector('.overlay');// Seleziono il contenitore con l'immagine ingrandita
+  const closeOverlayButton = document.getElementById('close-overlay');// Seleziono il pulsante per chiudere l'overlay
 
 // Aggiungo un event listener a ciascuna immagine nella galleria e utilizzo src per copiare il collegamento     
 galleryImages.forEach(image => {
@@ -31,28 +61,3 @@ closeOverlayButton.addEventListener('click', () => {
   overlay.classList.remove('d-block');
   });
 })
-
-// Creo una funzione per aggiungere una card dentro il contenitore row-card
-function printCard(postcard){
-
-// Destrutturo l'oggetto postcard per ottenere `title` e `url`  
-  let {title, url} = postcard;
-
-// Aggiungo una nuova card all'elemento HTML
-document.getElementById('row-card').innerHTML += 
-`<div class= "col col-lg-4 col-md-6 col-sm-12 mb-4 px-3">
-  <div class="card p-3 my-2 bg-white">
-      <div class="card-img-container">
-        <img src="img/pin.svg" class="pin" alt="pin">
-        <img src="${url}" class="card-img-top" alt="img" style="width: 100%; height: 100%;">
-      </div>
-      <div class="card-body">
-        <p class="card-text">${title}</p>
-      </div>  
-  </div>
-</div>`;
-}
-
-// L'obiettivo finale di questo codice è caricare le immagini da un'API e mostrarle come card.
-// Quando clicco un'immagine, questa viene mostrata ingrandita in un overlay.
-// L'overlay può essere chiuso con un pulsante.
